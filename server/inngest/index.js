4a3870a3b3e0b9a3a00c1c74ceb0357824959c67
Mohammed Nihal <mohdnihalmkl@gmail.com>
2025-06-27 09:38:18 +0530

    "Error Solve"

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -5,43 +5,57 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 //Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id:'sync-user-from-clerk'},
-    {event:'clerk/user.created'},
-    async ({event}) => {
-        const {id,first_name,last_name,email_addresses,image_url} = event.data
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        try {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + ' ' +last_name,
-            image:image_url
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
         }
         await User.create(userData)
+         } catch (error) {
+            res.status(500).json({ message: 'Internal server error' })
+        }
     }
 )
 // Inngest function to dalete user from database
 const syncUserDeletion = inngest.createFunction(
-    {id:'delete-user-from-clerk'},
-    {event:'clerk/user.deleted'},
-    async ({event}) => {
-       const {id} = event.data
-       await User.findByIdAndDelete(id)
+
+    { id: 'delete-user-from-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        try {
+            const { id } = event.data
+            await User.findByIdAndDelete(id)
+        } catch (error) {
+            res.status(500).json({ message: 'Internal server error' })
+        }
+
     }
 )
 // Inngest function to update user from database
 const syncUserUpdation = inngest.createFunction(
-    {id:'update-user-from-clerk'},
-    {event:'clerk/user.updated'},
-    async ({event}) => {
-        const {id,first_name,last_name,email_addresses,image_url} = event.data
-       const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + ' ' +last_name,
-            image:image_url
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        try {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
         }
-        await User.findByIdAndUpdate(id,userData)
+        await User.findByIdAndUpdate(id, userData)
+         } catch (error) {
+            res.status(500).json({ message: 'Internal server error' })
+        }
     }
 )
 
 
-export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
